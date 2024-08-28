230ee09d5103e329f938dc3a03c33a5917f7b788
RziankinS <98005611+RziankinS@users.noreply.github.com>
2024-08-29 01:44:51 +1000

    Add files via upload

---
+++ terraform/15.1/variables.tf
@@ -0,0 +1,39 @@
+variable "yandex_cloud_id" {
+  default = "b1ggp2bhrul82jpvs4rt"
+}
+
+variable "yandex_folder_id" {
+  default = "b1gh1p257m29u8c4p09p"
+}
+
+variable "centos-7" {
+  default = "fd837b0gcg6klm9d9nl6"
+}
+
+variable "zone" {
+  default = "ru-central1-b"  
+}
+
+variable "nat-instance-name" {
+  default = "nat-instance-vm1"
+}
+
+variable "nat-instance-image-id" {
+  default = "fd80mrhj8fl2oe87o4e1"
+}
+
+variable "nat-instance-ip" {
+  default = "192.168.10.254"
+}
+
+variable "domain" {
+  default = "netology.cloud"
+}
+
+variable "public-vm-name" {
+  default = "public-vm1"
+}
+
+variable "private-vm-name" {
+  default = "private-vm1"
+}
