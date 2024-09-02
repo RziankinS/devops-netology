5137d2a4ecbd12ae1e4942f9d6f0a6aa9337fc35
RziankinS <98005611+RziankinS@users.noreply.github.com>
2024-09-02 22:18:37 +1000

    Update network.tf

--- terraform/15.2/network.tf
+++ terraform/15.2/network.tf
@@ -5,6 +5,6 @@ resource "yandex_vpc_network" "network-dev" {
 resource "yandex_vpc_subnet" "subnet-public" {
   name = "public"
   zone = var.zone
-  network_id = yandex_vpc_network.network-dex.id
+  network_id = yandex_vpc_network.network-dev.id
   v4_cidr_blocks = [ "192.168.10.0/24" ]
 }
